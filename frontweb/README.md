028a8fb37cef630e6b3d52f8fbefa3c563659a54
blackawa <3104kurokawa@gmail.com>
2017-10-19 18:49:17 +0900

    :tada: awake application succeeded

---
+++ frontweb/README.md
@@ -0,0 +1,17 @@
+# frontweb
+[open-histor.io](https://open-histor.io) frontend application.
+
+## Development
+To turn up a development middleware, execute
+
+    docker-compose up -d
+
+To run application, execute
+
+    ./gradlew bootRun
+
+## Deployment
+
+To deploy application, execute
+
+    ???
--- frontweb/src/main/kotlin/io/openhistor/frontweb/Application.kt
+++ frontweb/src/main/kotlin/io/openhistor/frontweb/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class FrontwebApplication
+class Application
 
 fun main(args: Array<String>) {
-    SpringApplication.run(FrontwebApplication::class.java, *args)
+    SpringApplication.run(Application::class.java, *args)
 }
