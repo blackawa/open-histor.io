1e6abf5c6693a5a6b492cbdfc12b99f31a82cc89
blackawa <3104kurokawa@gmail.com>
2017-10-21 00:06:13 +0900

    :seedling: create new.html

---
+++ frontweb/src/main/kotlin/io/openhistor/frontweb/presentation/controller/AuthenticationController.kt
@@ -0,0 +1,10 @@
+package io.openhistor.frontweb.presentation.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class AuthenticationController {
+    @GetMapping("/users/session/new")
+    fun new(): String = "users/session/new"
+}
