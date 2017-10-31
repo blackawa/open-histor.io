3dd73ff8eec10a67e35a918a00d87dfbca4ad6e3
blackawa <3104kurokawa@gmail.com>
2017-10-31 20:24:52 +0900

    :recycle: change package

--- frontweb/src/main/kotlin/io/openhistor/frontweb/layer/presentation/controller/AuthenticationController.kt
+++ frontweb/src/main/kotlin/io/openhistor/frontweb/layer/presentation/controller/AuthenticationController.kt
@@ -1,4 +1,4 @@
-package io.openhistor.frontweb.presentation.controller
+package io.openhistor.frontweb.layer.presentation.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
